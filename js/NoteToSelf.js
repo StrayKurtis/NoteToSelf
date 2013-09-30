c0defdabaa009236974f62638e1dddf110b3ee37
StrayKurtis <kurtis001@gmail.com>
2013-09-30 18:08:20 -0500

    New version

---
+++ js/NoteToSelf.js
@@ -0,0 +1,259 @@
+var NoteToSelf = {
+    //Number of notes
+    count: 0,
+            
+    //Notes container
+    container: null,
+            
+    //Are we currently editing/adding a note?
+    editing: false,
+            
+    //Return our local storage object
+    storage: function () {
+        var storage = JSON.parse(localStorage.getItem('NoteToSelf')) || localStorage.setItem('NoteToSelf',JSON.stringify([]));
+        return storage;
+    },
+    
+    //Add a note container to the DOM(if we are able to)
+    addNote: function(noteObj){
+        if(this.editing !== true)
+        {
+            var that = this;
+            var noteId = this.count;
+            //Create note, paragraph inside of note, and close button
+            var note = document.createElement('article');
+            var noteText = document.createElement('p');
+
+            //Create save/edit/close buttons unordered list
+            var noteButtons = document.createElement('ul');
+            noteButtons.className = 'noteButtons';
+            
+            //Close button
+            var close = document.createElement('li');
+            close.className = 'close';
+            close.setAttribute('title','Delete Note');
+            close.addEventListener('click', function(e){
+                that.removeNote(e.target);
+            });
+
+            //edit button
+            var edit = document.createElement('li');
+            edit.className = 'edit';
+            edit.setAttribute('title','Edit Note');
+            edit.addEventListener('click', function(e){
+                if(this.className.match('disabled') === null && that.editing !== true)
+                {
+                    that.editing = true;
+                    that.editNote(e);
+                }
+            });
+
+            //Save button
+            var save = document.createElement('li');
+            save.className = 'save';
+            save.setAttribute('title','Save Changes');
+            save.addEventListener('click', function(e){
+                if(this.className.match('disabled') === null)
+                {
+                    that.editing = false;
+                    that.createNote(note);
+                }
+            });
+            
+            //append buttons to container
+            noteButtons.appendChild(save);
+            noteButtons.appendChild(edit);
+            noteButtons.appendChild(close);
+
+            note.className = 'note';
+            //If this is a new note, add textarea to note article
+            if(typeof noteObj === 'undefined')
+            {
+                this.editing = true;
+                var form = document.createElement('textarea');
+                form.setAttribute('id','noteText');
+                form.setAttribute('placeholder','Begin Typing your note here...');
+                noteText.appendChild(form);
+                edit.className += ' disabled';
+            }
+            else
+            {
+                //If its not a new note, display it regularly
+                noteText.innerHTML = noteObj.note;
+                save.className += ' disabled';
+            }
+            //Set the note id, add the text/textarea and the buttons
+            note.setAttribute('data-id',noteId);
+            note.appendChild(noteText);
+            note.appendChild(noteButtons);
+            
+            //Insert new note and set focus to textarea if applicable
+            this.container.insertBefore(note,this.container.firstChild);
+            if(this.editing === true){
+                document.getElementById('noteText').focus();
+            }
+            this.count++;
+        }
+    },
+    //Edit our note
+    editNote: function (e) {
+        var edit = e.target;
+        var save = e.target.previousSibling;
+        var para = e.target.parentNode.previousSibling;
+        var form = document.createElement('textarea');
+        form.setAttribute('id','noteText');
+        form.value = para.firstChild.nodeValue;
+        para.removeChild(para.firstChild);
+        para.appendChild(form);
+        
+        //Disable edit button, enable save button, set focus to textarea
+        edit.className += ' disabled';
+        save.className = 'save';
+        form.focus();
+    },
+    //Add or save note(dependent on circumstance)
+    createNote: function (noteNode) {
+        var store = this.storage();
+        var noteId = noteNode.getAttribute('data-id');
+        var txt = document.getElementById('noteText');
+        var save = noteNode.childNodes[1].childNodes[0];
+        var edit = save.nextSibling;
+        
+        //Textarea empty?
+        if(txt.value !== '')
+        {
+            //Create JSON note object
+            var newNote = {title: '', date: (new Date()).getTime(), note: txt.value};
+            if(typeof store[noteId] !== 'undefined')
+            {
+                //If this is an edit, change existing note
+                store[noteId] = newNote;
+            }
+            else
+            {
+                //...otherwise, push the new note
+                store.push(newNote);
+            }
+
+            localStorage.setItem('NoteToSelf', JSON.stringify(store));                
+            noteNode.firstChild.innerHTML = newNote.note;
+
+            save.className += ' disabled';
+            edit.className = 'edit';
+            this.editing = false;
+        }
+        else
+        {
+            alert('You can\'t save a blank note, silly!');
+        }
+    },
+    //Remove note from local storage and DOM
+    removeNote: function (el){
+        var store = this.storage();
+        var that = this;
+        var id = el.parentNode.getAttribute('data-id');
+        var note = el.parentNode.parentNode;
+        store.splice(id,1);
+        //CSS animation
+        note.style.opacity = 0;
+        setTimeout(function(){
+            that.container.removeChild(note);
+        },400);
+       localStorage.setItem('NoteToSelf', JSON.stringify(store));
+    },
+    //Insert notes into storage and DOM from database
+    loadFromDB: function (notes) {
+        var notes = JSON.parse(notes);
+        var store = this.storage();
+        for(var i = 0; i < notes.length; i++)
+        {
+            //Push note to storage object and add note to DOM
+            store.push(notes[i]);
+            this.addNote(notes[i]);
+        }
+        localStorage.setItem('NoteToSelf', JSON.stringify(store));
+    },
+    //Empty the notes from DOM and storage
+    clearNotes: function() {
+        this.count = 0;
+        for(var i = 0; i < this.container.childNodes.length; i++)
+        {
+            var node = this.container.childNodes[i];
+            if(node.nodeType === 1)
+            {
+                this.container.childNodes[i].style.opacity = 0;
+            }
+            setTimeout(function(){ NoteToSelf.container.innerHTML = ''; },500);
+            localStorage.setItem('NoteToSelf',JSON.stringify([]));
+        }
+    },
+    //App setup
+    initialize: function () {
+        this.container = document.getElementById('notesContainer');
+        var store = this.storage();
+        for (var i = 0; i < store.length; i++)
+        {
+            this.addNote(store[i]);
+        }
+    },
+    //Load notes from database via AJAX
+    loadDB: function () {
+        var request = new XMLHttpRequest();
+        var loadDlg = document.getElementById('loadInfo');
+        //Open request, set header to allow usage of $_POST
+        request.open('POST','php/user.php');
+        request.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+        //Animate dialog in
+        loadDlg.style.top = '100%';
+        loadDlg.style.opacity = '1';
+        request.onreadystatechange = function(){
+            if(request.readyState === 4)
+            {
+                var response = JSON.parse(request.responseText);
+                //Did it send us an error?
+                if(response.error)
+                {
+                    loadDlg.innerHTML = response.error;
+                }
+                else
+                {
+                    //..No? Success! Load 'em!
+                    loadDlg.innerHTML = 'Sucessfully loaded your notes!';
+                    NoteToSelf.loadFromDB(response);
+                }
+                //Animate dialog bubble
+                setTimeout(function(){
+                    loadDlg.style.top = '-500%';
+                    loadDlg.style.opacity = '0';
+                }, 2500);
+            }
+        };
+        //Send 'load' action
+        request.send('action=load');
+    },
+    //Save notes to database using AJAX
+    saveDB: function () {
+        var request = new XMLHttpRequest();
+        var saveDlg = document.getElementById('saveInfo');
+        request.open('POST','php/user.php');
+        request.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+        //Animate save dialog bubble in
+        saveDlg.style.top = '100%';
+        saveDlg.style.opacity = '1';
+        request.onreadystatechange = function(){
+            if(request.readyState === 4)
+            {
+               //Display response message
+               saveDlg.innerHTML = request.responseText;
+
+               //Animate dialog out
+                setTimeout(function(){
+                    saveDlg.style.top = '-500%';
+                    saveDlg.style.opacity = '0';
+                }, 2500);
+            }
+        };
+        //Send 'save' action along with our notes object
+        request.send('action=save&notes='+JSON.stringify(this.storage()));
+    }
+};
---
+++ php/user.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * userNotes Class
+ * Allows user to save notes to servers database
+ * 
+ * @author      Kurtis Key <Webmaster@KurtisDesigns.net>
+ * @copyright   Copyright (c) 2013, Kurtis Key
+ * @link        http://KurtisDesigns.ne
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class userNotes{
+
+    /**
+     * Database connection credentials
+     */
+    public $dbUser = 'user';
+    public $dbPass = 'password';
+    public $dbHost = 'localhost';
+    public $dbPort = null;
+    
+    /**
+     * Name of the database we will be connecting to
+     * @var string 
+     */
+    public $dbDatabase = 'notetoself';
+    
+    /**
+     * Data source name for connecting to PDO. @see self::generateDSN();
+     * @var string 
+     */
+    public $dsn;
+    
+    /**
+     * Holyds PDO object
+     * @var PDO 
+     */
+    public $conn;
+    
+    /**
+     * Construct and connect to our database
+     * @throws \Exception if unable to connect
+     */
+    public function __construct()
+    {
+        try {
+            $this->conn = new PDO($this->generateDSN(), $this->dbUser, $this->dbPass);
+        } catch (PDOException $e) {
+            throw new Exception('There was an error connecting to the database. ('.$e->getMessage().')');
+        }
+    }
+    
+    /**
+     * Generate DSN required for connecting to PDO
+     * @return string
+     */
+    protected function generateDSN()
+    {
+        $dsn = sprintf('mysql:dbname=%s;host=%s',$this->dbDatabase,$this->dbHost);
+        !is_null($this->dbPort) and $dsn .= ';port=' . $this->dbPort;
+        return $this->dsn = $dsn;
+    }
+    
+    /**
+     * Attempt to save notes if user does not have existing record, otherwise update
+     * @param string $id
+     * @param array $notes
+     * @return string
+     */
+    public function userSave($id, $notes)
+    {
+        #$notes = json_encode($notes);
+        if($this->userExists($id))
+        {
+            $query = 'UPDATE usernotes SET notes = :notes WHERE id = :id';
+            $bind = array(':notes'=>$notes,':id'=>$id);
+        }
+        else
+        {
+            $query = 'INSERT INTO usernotes(id, notes) VALUES(:id, :notes)';
+            $bind = array(':id'=>$id,':notes'=>$notes);
+        }
+        
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute($bind);
+        $response = $stmt->rowCount();
+        return ($response > 0 ? 'Successfully saved your notes to the servers database!' : 'We ran in to an issue saving your notes, please try again.');
+    }
+    
+    /**
+     * Check if user exists in database
+     * @param string $id
+     * @return mixed(obect|bool)
+     */
+    public function userExists($id)
+    {
+        $query = 'SELECT * FROM usernotes WHERE id = :id';
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute(array(':id'=>$id));
+        $count = $stmt->rowCount();
+        return ($count > 0 ? $stmt : FALSE);
+    }
+}
+
+if($_POST)
+{
+/**
+ * Grab post vars and ip for use as an ID
+ */
+$action = $_POST['action'];
+$notes = isset($_POST['notes']) ? $_POST['notes'] : NULL;
+$id = $_SERVER['REMOTE_ADDR'];
+
+/**
+ * Is this a legitimate request?
+ */
+if(in_array($action,array('save','load')))
+{
+    try{
+        /**
+         * Instantiate object. If the action is a save/update, return the string response. If the action
+         * is load then return results(error or notes from DB) in JSON format
+         */
+        $userNotes = new userNotes();
+        
+        //Save action?
+        if($action === 'save')
+        {
+            //Save, return response
+            $response = $userNotes->userSave($id,$notes);
+            echo $response;
+        }
+        else
+        {
+            //Load action? Check if user has notes saved already
+            $response = $userNotes->userExists($id);
+            if($response)
+            {
+                //If they do, grab them from the database, encode and return notes
+                $notes = $response->fetch(PDO::FETCH_OBJ);
+                //Older versions of PHP(<5.4?) needs to strip slashes from results before sending...
+                echo json_encode(stripslashes($notes->notes));
+            }else
+            {
+                //...otherwise, let them know thay have no saved notes!
+                print_r(json_encode(array('error'=>'Sorry, you don\'t seem to have any saved notes!')));
+            }
+        }
+        
+    } catch(Exception $e) {
+        //Catch exception and send error
+        print_r(json_encode(array('error'=>$e->getMessage())));
+    }
+}
+else
+{
+    echo 'Invalid action...what are you trying to do?!';
+}
+}
